bb85c22ed165410e9c869f6d6ee9fa999486cf4f
Mostafa-Haggag <57625420+Mostafa-Haggag@users.noreply.github.com>
2023-10-06 22:46:55 +0200

    changes for a small error

--- gaze/datasets/build.py
+++ gaze/datasets/build.py
@@ -12,14 +12,14 @@ def get_loader(name: str, root_dir: str, random_flag=False,depth_on=False,x_loss
         labels = os.path.join(root_dir, "train_annotations_release.txt" if is_train else "test_annotations_release.txt")
         # root_dir datasets/gazefollow_extended
         # labels they are coming from file 
-        dataset = GazeFollow(root_dir, labels, random_size=random_flag,depth_on=depth_on,x_loss=x_loss,input_size=input_size, output_size=output_size, is_test_set=not is_train,is_subsample_test_set=is_subsample_test_set,gaze_point_threshold=gaze_point_threshold,sigma=3)
+        dataset = GazeFollow(root_dir, labels, random_size=random_flag,depth_on=depth_on,x_loss=x_loss,input_size=input_size, output_size=output_size, is_test_set=not is_train,is_subsample_test_set=is_subsample_test_set,gaze_point_threshold=gaze_point_threshold,sigma=sigma)
         loader = DataLoader(
             dataset=dataset, batch_size=batch_size, shuffle=is_train, num_workers=num_workers, pin_memory=True
         )
     elif name == "videoattentiontarget":
         labels = os.path.join(root_dir, "annotations/train" if is_train else "annotations/test")
         dataset = VideoAttentionTargetImages(
-            root_dir, labels, random_size=random_flag,depth_on=depth_on,input_size=input_size, output_size=output_size, is_test_set=not is_train,sigma=3
+            root_dir, labels, random_size=random_flag,depth_on=depth_on,input_size=input_size, output_size=output_size, is_test_set=not is_train,sigma=sigma
         )
         loader = DataLoader(
             dataset=dataset, batch_size=batch_size, shuffle=is_train, num_workers=num_workers, pin_memory=True
@@ -27,7 +27,7 @@ def get_loader(name: str, root_dir: str, random_flag=False,depth_on=False,x_loss
     elif name == "mhug":
         labels = os.path.join(root_dir, "annotations/")
         dataset = MHUGImages(
-            root_dir, labels, random_size=random_flag,depth_on=depth_on,input_size=input_size, output_size=output_size,sigma=3
+            root_dir, labels, random_size=random_flag,depth_on=depth_on,input_size=input_size, output_size=output_size,sigma=sigma
         )
         loader = DataLoader(
             dataset=dataset, batch_size=batch_size, shuffle=is_train, num_workers=num_workers, pin_memory=True

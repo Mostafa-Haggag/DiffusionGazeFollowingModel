bdc18a1a1855b4a302d6a6d6e8632dd014af8e2d
Mostafa-Haggag <57625420+Mostafa-Haggag@users.noreply.github.com>
2023-10-17 00:46:39 +0200

    adding adam

--- optimizer.py
+++ optimizer.py
@@ -7,4 +7,4 @@ def get_optimizer(model, lr=2.5e-4,weight_decay=0):
                 diffusion_models_weights.append(param)
         else:
             backbone_weights.append(param)
-    return torch.optim.Adam([{'params':diffusion_models_weights},{'params':backbone_weights,'lr': lr}], lr=lr,weight_decay=weight_decay)
+    return torch.optim.AdamW([{'params':diffusion_models_weights},{'params':backbone_weights,'lr': lr}], lr=lr,weight_decay=weight_decay)
